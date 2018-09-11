c3e204334ec8a7779743b75741e1157d98fd71f7
Katherine Kato <katherine.a.kato@gmail.com>
2018-09-10 19:19:26 -0700

    Added day 4

---
+++ Day 4 - Product/src/index.js
@@ -0,0 +1,16 @@
+import style from "./scss/main.scss";
+
+const activeImage = document.querySelector(".product-image .active");
+const productImages = document.querySelectorAll(".image-list img");
+const navItem = document.querySelector('a.toggle-nav');
+
+function changeImage(e) {
+  activeImage.src = e.target.src;
+}
+
+function toggleNavigation(){
+  this.nextElementSibling.classList.toggle('active');
+}
+
+productImages.forEach(image => image.addEventListener("click", changeImage));
+navItem.addEventListener('click', toggleNavigation);
